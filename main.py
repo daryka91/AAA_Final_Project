522b911d75d63a4f6161eae903c6d6430b0a2c5e
Kozlova Darya <daryka.91@gmail.com>
2022-12-06 12:14:15 +0300

    dataclass Pizza

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-import enum
 import click
 from enum import Enum
-
-
-class Pizza:
-    """Parent class"""
-
-    def __init__(self, size: str, ingredients: list[str]):
-        # size = ['L', 'XL']
-        self.ingredients = ingredients
-        self.size = size
-
-    def dict(self):
-        print('\n', self.__class__.__name__, sep='\n')
-        print('', *self.ingredients, sep='\n-')
+from dataclasses import dataclass
 
 
 class Size(Enum):
@@ -21,32 +8,67 @@ class Size(Enum):
     XL = 'XL'
 
 
+@dataclass
+# Нам не нужно определять методы __eq__,
+# потому что декоратор dataclass автоматически добавляет их в определение
+# класса при вызове с order = True
+class Pizza:
+    """Parent class"""
+    size: Size
+    ingredients: list[str]
+
+    def dict(self):
+        print('', self.__class__.__name__, sep='\n')
+        print('', *self.ingredients, sep='\n-')
+
+
 class Margherita(Pizza):
 
-    def __init__(self, size: str):
-        super().__init__(size, ['tomato sauce', 'mozzarella', 'tomatoes'])
+    def __init__(self, size: Size):
+        self.ingredients = ['tomato sauce', 'mozzarella', 'tomatoes']
+        super().__init__(size, self.ingredients)
         self.__class__.__name__ += ' 🧀'
 
 
 class Pepperoni(Pizza):
 
-    def __init__(self, size: str):
-        super().__init__(size, ['tomato sauce', 'mozzarella', 'pepperoni'])
+    def __init__(self, size: Size):
+        self.ingredients = ['tomato sauce', 'mozzarella', 'pepperoni']
+        super().__init__(size, self.ingredients)
         self.__class__.__name__ += ' 🍕'
 
 
 class Hawaiian(Pizza):
 
-    def __init__(self, size: str):
-        super().__init__(size, ['tomato sauce', 'mozzarella', 'chicken',
-                                'pineapples'])
+    def __init__(self, size: Size):
+        self.ingredients = ['tomato sauce', 'mozzarella', 'chicken',
+                            'pineapples']
+        super().__init__(size, self.ingredients)
         self.__class__.__name__ += ' 🍍'
 
 
+# @click.group()
+# def cli():
+#     pass
+#
+#
+# @cli.command()
+# @click.option(' =delivery', default=False, is_flag=True)
+# @click.argument('pizza', nargs=1)
+# def order(pizza: str, delivery: bool):
+#     """Готовит и доставляет пиццу"""
+#     pass
+#
+# @cli.command()
+# def menu():
+#     """Выводит меню"""
+#     pass
+
 if __name__ == '__main__':
-    a = Margherita('L')
+    a = Margherita(Size.L)
     a.dict()
-    b = Pepperoni('XL')
+    b = Pepperoni(Size.XL)
     b.dict()
-    c = Hawaiian('L')
+    c = Hawaiian(Size.L)
     c.dict()
+    print(b == c)
